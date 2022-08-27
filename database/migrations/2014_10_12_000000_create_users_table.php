67b51cd3db060bf58fffc1d55f2d8318cbbde20b
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2022-08-27 08:26:37 +0100

    Configured the user's migration, factory and seeder.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
